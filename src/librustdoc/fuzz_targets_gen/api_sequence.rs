de5531ba63ff7c808a5dabc3e1e511a53efd8d8f
yxz <1263522794@qq.com>
2023-06-04 21:44:53 +0800

    基本的泛型已经支持了

--- src/librustdoc/fuzz_targets_gen/api_sequence.rs
+++ src/librustdoc/fuzz_targets_gen/api_sequence.rs
@@ -380,11 +380,7 @@ impl ApiSequence {
     }
 
     pub(crate) fn _is_moved(&self, index: usize) -> bool {
-        if self._moved.contains(&index) {
-            true
-        } else {
-            false
-        }
+        if self._moved.contains(&index) { true } else { false }
     }
 
     pub(crate) fn _insert_move_index(&mut self, index: usize) {
@@ -400,11 +396,7 @@ impl ApiSequence {
     }
 
     pub(crate) fn _is_fuzzable_need_mut_tag(&self, index: usize) -> bool {
-        if self._fuzzable_mut_tag.contains(&index) {
-            true
-        } else {
-            false
-        }
+        if self._fuzzable_mut_tag.contains(&index) { true } else { false }
     }
 
     pub(crate) fn _insert_function_mut_tag(&mut self, index: usize) {
@@ -412,11 +404,7 @@ impl ApiSequence {
     }
 
     pub(crate) fn _is_function_need_mut_tag(&self, index: usize) -> bool {
-        if self._function_mut_tag.contains(&index) {
-            true
-        } else {
-            false
-        }
+        if self._function_mut_tag.contains(&index) { true } else { false }
     }
 
     pub(crate) fn set_unsafe(&mut self) {
